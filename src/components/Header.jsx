56d563678f05dbdff6a902ce3ffed6e73b916cb2
unknown <justicec566@gmail.com>
2025-02-21 03:17:35 -0800

    started working

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+   <header id='header' className='header fixed-top d-flex align-items-center'>
+header
+   </header>
+  )
+}
+
+export default Header
